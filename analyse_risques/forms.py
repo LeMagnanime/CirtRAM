56c6c3cdad2124a4969486a6d15faec69f019c5d
etali mathias <60927022+etali-m@users.noreply.github.com>
2024-05-18 19:00:29 +0100

    Update forms.py

--- analyse_risques/forms.py
+++ analyse_risques/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm
+from django import forms
 from .models import *
 
 #formulaire pour l'ajout des actifs
@@ -6,6 +7,16 @@ class AssetForm(ModelForm):
     class Meta:
         model = Asset
         fields = '__all__'
+
+        widgets = {  
+                'actif' : forms.Select(attrs={'class':'form-control'}),
+                'description': forms.TextInput(attrs={'class':'form-control'}),
+                'valeur_unitaire_actif': forms.NumberInput(attrs={'class':'form-control'}),
+                'cout_installation': forms.NumberInput(attrs={'class':'form-control'}),
+                'cout_entretien': forms.NumberInput(attrs={'class':'form-control'}),
+                'va': forms.NumberInput(attrs={'class':'form-control'}),
+                'valeur_indisponibilite': forms.NumberInput(attrs={'class':'form-control'}),
+        }
 
 class AnalyseForm(ModelForm):
     class Meta:
