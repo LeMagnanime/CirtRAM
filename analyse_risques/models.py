ed72b6f11cd1ecccbcf12eeb10c32c89f7c1879e
etali mathias <60927022+etali-m@users.noreply.github.com>
2024-05-18 18:59:59 +0100

    Update models.py

--- analyse_risques/models.py
+++ analyse_risques/models.py
@@ -15,11 +15,11 @@ class Asset(models.Model):
         verbose_name = "Actif"
     actif = models.ForeignKey(TypeActif, on_delete=models.CASCADE)
     description = models.CharField(max_length=200)
-    Valeur_unitaire_actif = models.IntegerField()
-    Cout_installation = models.IntegerField()
-    Cout_entretien = models.IntegerField()
-    VA = models.IntegerField()
-    Valeur_indisponibilité = models.IntegerField()
+    valeur_unitaire_actif = models.IntegerField()
+    cout_installation = models.IntegerField()
+    cout_entretien = models.IntegerField()
+    va = models.IntegerField()
+    valeur_indisponibilite = models.IntegerField()
     
     def __str__(self):
         return f"{self.actif}"
@@ -36,14 +36,14 @@ class Vunlerabilite(models.Model):
 
 class Analyse(models.Model):		 		
     actif = models.ForeignKey(Asset, on_delete=models.CASCADE)		
-    Vulnérabilité = models.ForeignKey(Vunlerabilite, on_delete=models.CASCADE)
-    Menaces = models.CharField(max_length=200) 
-    Risque = models.CharField(max_length=200)
-    Probabilité_occurrence = models.FloatField(max_length=200)
-    Impact_C = models.FloatField(max_length=200)
-    Impact_I = models.FloatField(max_length=200)
-    Impact_D = models.FloatField(max_length=200)
-    Criticité = models.FloatField(max_length=200)
+    vulnerabilite = models.ForeignKey(Vunlerabilite, on_delete=models.CASCADE)
+    menaces = models.CharField(max_length=200) 
+    misque = models.CharField(max_length=200)
+    probabilite_occurrence = models.FloatField(max_length=200)
+    impact_c = models.FloatField(max_length=200)
+    impact_i = models.FloatField(max_length=200)
+    impact_d = models.FloatField(max_length=200)
+    criticite = models.FloatField(max_length=200)
 
     def __str__(self):
         return self.actif
